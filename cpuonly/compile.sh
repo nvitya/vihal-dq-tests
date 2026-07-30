40f491d319b42a5442c545306cfad188b210d8bf
Viktor Nagy <nvitya@gmail.com>
2026-07-30 21:50:39 +0200

    modularized armm startup + vector table

--- cpuonly/compile.sh
+++ cpuonly/compile.sh
@@ -10,6 +10,8 @@ clang++ --target=thumbv7em-none-eabihf -fuse-ld=lld -nostdlib \
   -Xlinker "--library-path=$VIHAL/core/ld" \
   -Xlinker "--script=$VIHAL/armm/stm32/f7/STM32F750x8_ram.ld" \
   .dqbuild/arm_m7f-bare/local/cpuonly.o \
+  .dqbuild/arm_m7f-bare/local/armm_startup.o \
+  .dqbuild/arm_m7f-bare/local/armm_vector_table.o \
   -o cpuonly.elf
 
 llvm-size --format=sysv --radix=16 cpuonly.elf
