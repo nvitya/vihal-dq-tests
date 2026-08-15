52b962a5133d7e7a6b143c74753dcc8f2f03d0a6
Viktor Nagy <nvitya@gmail.com>
2026-08-15 12:37:08 +0200

    working blinking leds with separated board/board_pins module

--- cpuonly/compile.sh
+++ cpuonly/compile.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -e
+#set -e
 
-DQC=/workvc/dq-lang/build/dq-comp
-VIHAL=/workvc/vihal-dq
+#DQC=/workvc/dq-lang/build/dq-comp
+#VIHAL=/workvc/vihal-dq
 
-$DQC --target=arm_m7f-bare -O0 -g cpuonly.dq
+#$DQC --target=arm_m7f-bare -O0 -g cpuonly.dq
 
-clang++ --target=thumbv7em-none-eabihf -fuse-ld=lld -nostdlib \
-  -Xlinker "--library-path=$VIHAL/core/ld" \
-  -Xlinker "--script=$VIHAL/armm/stm32/f7/STM32F750x8_ram.ld" \
-  .dqbuild/arm_m7f-bare/local/cpuonly.o \
-  .dqbuild/arm_m7f-bare/local/armm_startup.o \
-  .dqbuild/arm_m7f-bare/local/armm_vector_table.o \
-  -o cpuonly.elf
+#clang++ --target=thumbv7em-none-eabihf -fuse-ld=lld -nostdlib \
+#  -Xlinker "--library-path=$VIHAL/core/ld" \
+#  -Xlinker "--script=$VIHAL/armm/stm32/f7/STM32F750x8_ram.ld" \
+#  .dqbuild/arm_m7f-bare/local/cpuonly.o \
+#  .dqbuild/arm_m7f-bare/local/armm_startup.o \
+#  .dqbuild/arm_m7f-bare/local/armm_vector_table.o \
+#  -o cpuonly.elf
 
-llvm-size --format=sysv --radix=16 cpuonly.elf
+llvm-size --format=sysv --radix=16 cpuonly_stm32f746.elf
